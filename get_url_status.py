70a3889c43b3751a53fafee7006ad7a5838c97f1
Iris Blackburn <pomoluese@gmail.com>
2017-05-30 23:33:05 -0700

    initial commit

---
+++ get_url_status.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+
+def main():
+    # get filename
+    file = input('Enter file path: ')
+
+    with open(file, 'r' ) as url_file:
+      urls = url_file.read().split(',')
+      results = ""
+      #timeout after 30 minutes
+      timeout = time.time() + 60*1
+    #   print("Starting to ping urls: " + time.ctime())
+      while time.time() < timeout:
+        #ping URL and save response to a string.
+        results = ping_urls(urls, results)
+        #sleep 5 minutes
+        time.sleep(10)
+    # print("Done pinging urls: " + time.ctime())
+    print(results)
+
+def ping_urls(urls, results):
+    for url in urls:
+        try:
+            r = requests.get(url)
+            results += url + "'s status: " + str(r.status_code) + "\n"
+        except Exception as e:
+            print("Error occurred while requesting the url " + url + "\n")
+            print(e)
+    return results
+
+if __name__ == "__main__":
+    main()
