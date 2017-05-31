ac011901c9045ef7555eaa7a6dec8452af199ae0
Iris Blackburn <pomoluese@gmail.com>
2017-05-31 09:23:48 -0700

    Replacing initial commit with proper timing and fixed error checking on file name

--- get_url_status.py
+++ get_url_status.py
@@ -5,20 +5,25 @@ import time
 def main():
     # get filename
     file = input('Enter file path: ')
-
-    with open(file, 'r' ) as url_file:
-      urls = url_file.read().split(',')
-      results = ""
-      #timeout after 30 minutes
-      timeout = time.time() + 60*1
-    #   print("Starting to ping urls: " + time.ctime())
-      while time.time() < timeout:
-        #ping URL and save response to a string.
-        results = ping_urls(urls, results)
-        #sleep 5 minutes
-        time.sleep(10)
-    # print("Done pinging urls: " + time.ctime())
-    print(results)
+    valid = False
+    while not valid:
+        try:
+            with open(file, 'r' ) as url_file:
+              urls = url_file.read().split(',')
+              results = ""
+              #timeout after 30 minutes
+              timeout = time.time() + 60*30
+            #   print("Starting to ping urls: " + time.ctime())
+              while time.time() < timeout:
+                #ping URL and save response to a string.
+                results = ping_urls(urls, results)
+                #sleep 5 minutes
+                time.sleep(300)
+            # print("Done pinging urls: " + time.ctime())
+            print(results)
+            valid = True
+        except:
+            file = input('Incorrect file, please try again: ')
 
 def ping_urls(urls, results):
     for url in urls:
